883d14e18493829791436a991047e52ae1136ede
Shandilya <shubham.shandilya@esko.com>
2023-11-14 01:38:42 +0530

    episode-03

--- app.js
+++ app.js
@@ -2,46 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const TitleComponent = () => <h1 id="title">This is PageTitle</h1>;
+const numberOfVisits = 863;
+const numberOfVisitsPara = <p>number of Visits: {numberOfVisits}</p>;
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyx: "abs", className: "heading" },
-  "Hello world from react!"
+const HeadingComponent = () => (
+  <div id="container">
+    {TitleComponent()}
+    <TitleComponent></TitleComponent>
+    <TitleComponent />
+    {numberOfVisitsPara}
+    <h2>{console.log("This is JS")}</h2>
+    <h2 id="heading" className="heading">
+      This is a heading using JSX
+    </h2>
+  </div>
 );
 
-const smallHeading = React.createElement(
-  "h3",
-  { id: "smallHeading" },
-  "What's up!"
-);
-
-const para1 = React.createElement(
-  "p",
-  { id: "content1" },
-  "This is a paragraph one"
-);
-const para2 = React.createElement(
-  "p",
-  { id: "content2" },
-  "This is a paragraph two"
-);
-const child = React.createElement("div", { id: "child" }, [
-  heading,
-  smallHeading,
-]);
-
-const child2 = React.createElement("div", { id: "child2" }, [para1, para2]);
-const parent = React.createElement("div", { id: "parent" }, [child, child2]);
-console.log(parent);
-
-root.render(parent);
-
-const root2 = ReactDOM.createRoot(document.getElementById("footer"))
-
-const footer = React.createElement(
-    "h4",
-    {},
-    "Footer"
-)
-
-root2.render(footer)
+root.render(<HeadingComponent />);
